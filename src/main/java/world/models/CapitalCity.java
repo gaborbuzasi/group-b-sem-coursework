463497030fc47f3bcab5109e0247a0ba96b48e74
Gabor Buzasi <gabor@sharein.com>
2019-02-14 14:38:30 +0100

    LIMIT query in SQL does not work yet

---
+++ src/main/java/world/models/CapitalCity.java
@@ -0,0 +1,7 @@
+package world.models;
+
+public class CapitalCity {
+    public String Name;
+    public String Country;
+    public int Population;
+}
---
+++ src/main/java/world/models/City.java
@@ -0,0 +1,8 @@
+package world.models;
+
+public class City {
+    public String Name;
+    public String CountryCode;
+    public String District;
+    public int Population;
+}
