0234d8e72ef30e75028957081724c4213e51ff59
PrincessC96 <40417334@live.napier.ac.uk>
2019-02-17 10:48:16 +0000

    City class created

---
+++ src/main/java/world/models/City.java
@@ -0,0 +1,24 @@
+package world.models;
+
+public class City {
+    /**
+     * City Code
+     */
+    public String Code;
+
+    /**
+     * City Name
+     */
+    public String Name;
+
+    /**
+     * City Country
+     */
+    public String Country;
+
+    /**
+     *  City District
+     */
+
+    public String District;
+}
