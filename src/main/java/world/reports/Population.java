ee016418b2eaecf63987a5a03c7052e35f5296de
40417336 <40417336@napier.ac.uk>
2019-02-26 16:09:39 +0000

    Added stub for population report

--- src/main/java/world/reports/Population.java
+++ src/main/java/world/reports/Population.java
@@ -1,4 +1,11 @@
 package world.reports;
 
+import java.util.List;
+
 public class Population {
+    public static List<world.models.Population> getPopulationOfContinent(String continent)
+    {
+        //TODO: add implementation
+        return null;
+    }
 }
