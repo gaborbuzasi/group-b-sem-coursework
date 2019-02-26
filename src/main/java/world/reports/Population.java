45f3822aab03c2be7ae5e9b76c8eab16fe13b043
40417336 <roberthawker16@gmail.com>
2019-02-26 16:22:00 +0000

    Added stub for population report

--- src/main/java/world/reports/Population.java
+++ src/main/java/world/reports/Population.java
@@ -1,11 +1,38 @@
 package world.reports;
 
+import world.connection.Connection;
+import world.models.Country;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Population {
     public static List<world.models.Population> getPopulationOfContinent(String continent)
     {
-        //TODO: add implementation
-        return null;
+        try
+        {
+            // Initializes a connection to the database
+            Connection conFactory = new Connection();
+            Statement stmt = conFactory.conn.createStatement();
+
+            String strSelect = "SELECT c.Code, c.Name, c.Continent, c.Region, c.Population, ci.Name AS Capital " +
+                    "FROM country c " +
+                    "LEFT JOIN city ci on c.Capital = ci.ID " +
+                    "ORDER BY c.Population DESC";
+
+            List<Country> result = new ArrayList<>();
+
+            ResultSet rSet = stmt.executeQuery(strSelect);
+
+            return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
     }
 }
