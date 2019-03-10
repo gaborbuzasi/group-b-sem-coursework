f43165ac3478091cb701806014b3e61d22392606
PrincessC96 <40417334@live.napier.ac.uk>
2019-03-10 20:29:20 +0000

    ID47

--- src/main/java/world/reports/Population.java
+++ src/main/java/world/reports/Population.java
@@ -3,6 +3,7 @@ package world.reports;
 import world.connection.Connection;
 import world.models.Country;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -24,4 +25,46 @@ public class Population {
             return null;
         }
     }
+
+
+    public static List<world.models.Population>
+    {
+        try
+        {
+            // Initializes a connection to the database
+            Connection conFactory = new Connection();
+            Statement stmt = conFactory.conn.createStatement();
+
+            String strSelect = "SELECT DISTINCT ((SUM(c.Population)-(" +
+                    "SELECT DISTINCT SUM(ci.Population) " +
+                    "FROM country c " +
+                    "LEFT JOIN city ci on c.Capital = ci.ID " +
+                    "GROUP BY c.Continent ) AS PopulationNotInCities)" +
+            ("SELECT DISTINCT SUM(ci.Population) AS Population, c.Continent " +
+                    "FROM country c " +
+                "LEFT JOIN city ci on c.Capital = ci.ID " +
+                "GROUP BY c.Continent )" +
+                    "FROM country c " +
+                    "LEFT JOIN city ci on c.Capital = ci.ID " +
+                    "GROUP BY c.Continent ");
+
+
+            PreparedStatement stmt = conFactory.conn.prepareStatement(strSelect);
+            stmt.setInt(1, numberOfRows);
+
+            ResultSet rSet = stmt.executeQuery();
+
+            return processResults(rSet);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
 }
+
+
+
+
